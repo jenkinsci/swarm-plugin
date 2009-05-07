7e7bfd2c6a597345abe8c5169a2e0c71288e62c0
kohsuke <kohsuke@dev.java.net>
2009-05-07 01:34:20 +0000

    added the config screen

--- plugin/src/main/java/hudson/plugins/swarm/SwarmSlave.java
+++ plugin/src/main/java/hudson/plugins/swarm/SwarmSlave.java
@@ -5,14 +5,14 @@ import hudson.model.Descriptor.FormException;
 import hudson.model.Hudson;
 import hudson.model.Node;
 import hudson.model.Slave;
+import hudson.model.TaskListener;
 import hudson.slaves.ComputerLauncher;
 import hudson.slaves.EphemeralNode;
 import hudson.slaves.JNLPLauncher;
-import hudson.slaves.NodeDescriptor;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.RetentionStrategy;
 import hudson.slaves.SlaveComputer;
-import hudson.util.StreamTaskListener;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 import java.io.IOException;
 import java.util.Collections;
@@ -26,6 +26,7 @@ import java.util.Collections;
  * @author Kohsuke Kawaguchi
  */
 public class SwarmSlave extends Slave implements EphemeralNode {
+    @DataBoundConstructor
     public SwarmSlave(String name, String nodeDescription, String remoteFS, String numExecutors, String label) throws IOException, FormException {
         super(name, nodeDescription, remoteFS, numExecutors, Mode.NORMAL, label,
                 SELF_CLEANUP_LAUNCHER, RetentionStrategy.NOOP, Collections.<NodeProperty<?>>emptyList());
@@ -36,7 +37,7 @@ public class SwarmSlave extends Slave implements EphemeralNode {
     }
 
     @Extension
-    public static final class DescriptorImpl extends NodeDescriptor {
+    public static final class DescriptorImpl extends SlaveDescriptor {
         public String getDisplayName() {
             return "Swarm Slave";
         }
@@ -55,7 +56,7 @@ public class SwarmSlave extends Slave implements EphemeralNode {
      */
     private static final JNLPLauncher SELF_CLEANUP_LAUNCHER = new JNLPLauncher() {
         @Override
-        public void afterDisconnect(SlaveComputer computer, StreamTaskListener listener) {
+        public void afterDisconnect(SlaveComputer computer, TaskListener listener) {
             try {
                 Hudson.getInstance().removeNode(computer.getNode());
             } catch (IOException e) {
