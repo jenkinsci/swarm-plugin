988a06b222f2331788e214f6e5d7d6a40c77fa26
sylv704 <sylvain.baudoin@gmail.com>
2017-12-16 16:06:29 +0100

    Add ability to download the client directory from the plugin with the URL http://jenkins_server:port/swarm/swarm-client.jar

---
+++ plugin/src/main/java/hudson/plugins/swarm/DownloadClientAction.java
@@ -0,0 +1,30 @@
+package hudson.plugins.swarm;
+
+import hudson.Extension;
+import hudson.model.UnprotectedRootAction;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+@Extension
+public class DownloadClientAction implements UnprotectedRootAction {
+    public String getIconFileName() {
+        return null;
+    }
+
+    public String getDisplayName() {
+        return null;
+    }
+
+    public String getUrlName() {
+        return "swarm";
+    }
+
+    // serve static resources
+    public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
+        Jenkins.getActiveInstance().getPlugin("swarm").doDynamic(req, rsp);
+    }
+}
