4fa5ac5fad71e4245138cca761fa3545a18b0788
sylv704 <sylvain.baudoin@gmail.com>
2017-12-27 09:46:29 +0100

    Add annotation @Restricted(NoExternalUse.class) as requested by the plugin owner

--- plugin/src/main/java/hudson/plugins/swarm/DownloadClientAction.java
+++ plugin/src/main/java/hudson/plugins/swarm/DownloadClientAction.java
@@ -5,6 +5,8 @@ import hudson.model.UnprotectedRootAction;
 import jenkins.model.Jenkins;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 import javax.servlet.ServletException;
 import java.io.IOException;
@@ -24,6 +26,7 @@ public class DownloadClientAction implements UnprotectedRootAction {
     }
 
     // serve static resources
+    @Restricted(NoExternalUse.class)
     public void doDynamic(StaplerRequest req, StaplerResponse rsp) throws IOException, ServletException {
         Jenkins.getActiveInstance().getPlugin("swarm").doDynamic(req, rsp);
     }
