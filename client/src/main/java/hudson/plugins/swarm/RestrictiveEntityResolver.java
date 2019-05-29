f934a8d9f781e7eb430fc4362ef6cf5da1d2ce9a
Basil Crow <me@basilcrow.com>
2019-05-29 08:48:11 -0700

    Strengthen XXE checks (#90)

---
+++ client/src/main/java/hudson/plugins/swarm/RestrictiveEntityResolver.java
@@ -0,0 +1,35 @@
+package hudson.plugins.swarm;
+
+import java.io.IOException;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * An EntityResolver that will fail to resolve any entities. Useful in preventing External XML
+ * Entity injection attacks.
+ */
+@Restricted(NoExternalUse.class)
+public final class RestrictiveEntityResolver implements EntityResolver {
+
+    public static final RestrictiveEntityResolver INSTANCE = new RestrictiveEntityResolver();
+
+    private RestrictiveEntityResolver() {
+        // prevent multiple instantiation.
+        super();
+    }
+
+    /** Throws a SAXException if this tried to resolve any entity. */
+    @Override
+    public InputSource resolveEntity(String publicId, String systemId)
+            throws SAXException, IOException {
+        throw new SAXException(
+                "Refusing to resolve entity with publicId("
+                        + publicId
+                        + ") and systemId ("
+                        + systemId
+                        + ")");
+    }
+}
