798c75974e70a5da9baf870c47db7591361f83c2
Basil Crow <me@basilcrow.com>
2020-05-04 10:34:14 -0700

    Update parent POMs

--- plugin/src/test/java/hudson/plugins/swarm/test/TestUtils.java
+++ plugin/src/test/java/hudson/plugins/swarm/test/TestUtils.java
@@ -1,9 +1,9 @@
 package hudson.plugins.swarm.test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import hudson.model.Computer;
