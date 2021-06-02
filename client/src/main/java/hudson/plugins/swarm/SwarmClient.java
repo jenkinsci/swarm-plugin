a82051e56baf5f89afe6eb7ed28d02e9fc7eaed8
Nik Reiman <nre@ableton.com>
2021-06-02 15:54:18 +0200

    Rename remoteFsRoot variable -> fsroot (#337)
    Previously, this was the only option which had an internal variable
    name which differed from the exposed option name. This could be
    confusing for users wanting to use the new YAML config file format,
    since it meant that they would need to specify `remoteFsRoot` there,
    and look at the source code to figure out this option name.

--- client/src/main/java/hudson/plugins/swarm/SwarmClient.java
+++ client/src/main/java/hudson/plugins/swarm/SwarmClient.java
@@ -100,7 +100,7 @@ public class SwarmClient {
     public SwarmClient(Options options) {
         this.options = options;
         if (!options.disableClientsUniqueId) {
-            this.hash = hash(options.remoteFsRoot);
+            this.hash = hash(options.fsroot);
         } else {
             this.hash = "";
         }
@@ -222,7 +222,7 @@ public class SwarmClient {
             String workDirPath =
                     options.workDir != null
                             ? options.workDir.getPath()
-                            : options.remoteFsRoot.getPath();
+                            : options.fsroot.getPath();
             args.add("-workDir");
             args.add(workDirPath);
 
@@ -433,7 +433,7 @@ public class SwarmClient {
                                 + options.name
                                 + "&executors="
                                 + options.executors
-                                + param("remoteFsRoot", options.remoteFsRoot.getAbsolutePath())
+                                + param("remoteFsRoot", options.fsroot.getAbsolutePath())
                                 + param("description", options.description)
                                 + param("labels", sMyLabels)
                                 + toolLocationBuilder
