c94070b01956cfe3f9e6528f3ebf33f8fcb030d6
Frank Genois <francois.genois@native-instruments.com>
2025-11-26 11:00:01 -0500

    Fix race condition and zombie channels in SwarmLauncher
    - Synchronize on computer object to prevent duplicate afterDisconnect() calls
    - Explicitly close channel before node removal
    - Add double-check locking after acquiring synchronization lock
    - Log when node already removed by another thread

--- plugin/src/main/java/hudson/plugins/swarm/SwarmLauncher.java
+++ plugin/src/main/java/hudson/plugins/swarm/SwarmLauncher.java
@@ -6,6 +6,7 @@ import hudson.Functions;
 import hudson.model.Descriptor;
 import hudson.model.Slave;
 import hudson.model.TaskListener;
+import hudson.remoting.Channel;
 import hudson.slaves.ComputerLauncher;
 import hudson.slaves.JNLPLauncher;
 import hudson.slaves.SlaveComputer;
@@ -35,26 +36,51 @@ public class SwarmLauncher extends JNLPLauncher {
         Slave node = computer.getNode();
         if (node != null) {
             String nodeName = node.getNodeName();
-            try {
-                // Don't remove the node object if we've disconnected, if the node doesn't want to
-                // be removed
-                KeepSwarmClientNodeProperty keepClientProp = node.getNodeProperty(KeepSwarmClientNodeProperty.class);
-
-                // We use the existance of the node property on the node itself as a boolean check
-                if (keepClientProp == null) {
-                    LOGGER.log(Level.INFO, "Removing Swarm Node for computer [{0}]", nodeName);
-                    Jenkins.get().removeNode(node);
-                } else {
-                    listener.getLogger().printf("Skipping removal of Node for computer [%1$s]", nodeName);
-                    LOGGER.log(Level.INFO, "Skipping removal of Node for computer [{0}]", nodeName);
+            
+            // Synchronize on computer to prevent duplicate afterDisconnect() calls from racing
+            synchronized (computer) {
+                // Double-check node still exists after acquiring lock
+                node = computer.getNode();
+                if (node == null) {
+                    listener.getLogger().printf("Node for computer [%1$s] was already removed by another thread%n", nodeName);
+                    LOGGER.log(Level.INFO, "Node for computer [{0}] was already removed by another thread", nodeName);
+                    return;
                 }
-            } catch (IOException e) {
-                Functions.printStackTrace(e, listener.error("Failed to remove node [%1$s]", nodeName));
-                LOGGER.log(
-                        Level.WARNING,
-                        String.format(
-                                "Failed to remove node [%1$s] %n%2$s",
-                                nodeName, Functions.printThrowable(e).trim()));
+                
+                // Explicitly close the channel to prevent zombie channels
+                Channel channel = computer.getChannel();
+                if (channel != null) {
+                    LOGGER.log(Level.FINE, "Explicitly closing channel for computer [{0}]", nodeName);
+                    try {
+                        channel.close();
+                    } catch (IOException e) {
+                        LOGGER.log(Level.WARNING, "Failed to close channel for computer [{0}]: {1}", 
+                                new Object[]{nodeName, e.getMessage()});
+                        // Continue with node removal even if channel close fails
+                    }
+                }
+                
+                try {
+                    // Don't remove the node object if we've disconnected, if the node doesn't want to
+                    // be removed
+                    KeepSwarmClientNodeProperty keepClientProp = node.getNodeProperty(KeepSwarmClientNodeProperty.class);
+
+                    // We use the existance of the node property on the node itself as a boolean check
+                    if (keepClientProp == null) {
+                        LOGGER.log(Level.INFO, "Removing Swarm Node for computer [{0}]", nodeName);
+                        Jenkins.get().removeNode(node);
+                    } else {
+                        listener.getLogger().printf("Skipping removal of Node for computer [%1$s]", nodeName);
+                        LOGGER.log(Level.INFO, "Skipping removal of Node for computer [{0}]", nodeName);
+                    }
+                } catch (IOException e) {
+                    Functions.printStackTrace(e, listener.error("Failed to remove node [%1$s]", nodeName));
+                    LOGGER.log(
+                            Level.WARNING,
+                            String.format(
+                                    "Failed to remove node [%1$s] %n%2$s",
+                                    nodeName, Functions.printThrowable(e).trim()));
+                }
             }
         } else {
             listener.getLogger().printf("Node for computer [%1$s] appears to have been removed already%n", computer);
